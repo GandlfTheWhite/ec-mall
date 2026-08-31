217f6054c209afb15f809fb67e6df1ff3d93ae20
GandlfTheWhite <147932116+GandlfTheWhite@users.noreply.github.com>
2026-08-31 14:40:23 +0900

    Enable scheduling in GulimailPlusApplication

--- src/main/java/com/zyd/ecmall/GulimailPlusApplication.java
+++ src/main/java/com/zyd/ecmall/GulimailPlusApplication.java
@@ -2,8 +2,10 @@ package com.zyd.ecmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  
 public class GulimailPlusApplication {
 
     public static void main(String[] args) {
